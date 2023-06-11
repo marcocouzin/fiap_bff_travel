87021bf965a99a3e8c6077e0146c2756062dd12d
marcocouzin <mcouzin@gmail.com>
2023-06-11 13:46:34 -0300

    Docker set up

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
-COPY fiap_bff_travel.jar fiap_bff_travel.jar
+COPY deploy/fiap_bff_travel-0.0.1-SNAPSHOT.jar fiap_bff_travel.jar
 ENTRYPOINT ["java","-jar","/fiap_bff_travel.jar"]
--- src/main/java/br/com/fiap/fiap_bff_travel/controller/TravelController.java
+++ src/main/java/br/com/fiap/fiap_bff_travel/controller/TravelController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("travel")
 public class TravelController {
 
-    @RequestMapping("/")
+    @RequestMapping
     public String greeting() {
         return "Hello World";
     }
